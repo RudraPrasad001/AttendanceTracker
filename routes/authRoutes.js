d32a7088c120dd38f6a166b3b64d7cb2fe9ace90
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-06-21 13:31:53 +0530

    Basic Changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
 import connections from "../db/dbConnection.js";
 import {config} from "dotenv";
 config();
@@ -9,15 +9,25 @@ authRouter.post("/register",async(req,res)=>{
     if(!data){
         return res.json({message:"Error!Cannot get the data"});
     }
-    if(!data.name.trim() || !data.email.trim() ||!data.password.trim()){
-        return res.json({message:"Fill all the fields"})
+    if(!data.name.trim() || !data.phone.trim()||data.phone.length!==10){
+        return res.json({message:"Fill all the fields Correctly"});
     }
-    const salt = bcrypt.genSaltSync(Number.parseInt(process.env.bcryptKey));
-    const hashedPass = bcrypt.hashSync(data.password, salt);
-
+      //trying to see if the user already exists with the phone number;
     try{
-    connections.query(`insert into users(name,email,password) values('${data.name}','${data.email}','${hashedPass}')`)
-    .then(()=>{console.log("New user Created "+hashedPass);res.json({message:"New user Successfully created"})})
+        let query = `select id from users where id=${data.phone}`;
+        const result = await connections.query(query);
+        if(!result) throw new Error("Something broke");
+        if(result.rows[0]?.id){{throw new Error("Id already registered")}}
+    }
+    catch(e){
+        console.error(e.message);
+        return;
+    }
+    const defaultPassword = process.env.DEFAULTPASSWORD;
+  
+    try{
+    connections.query(`insert into users(id,name,password) values('${new Number(data.phone)}','${data.name}','${defaultPassword}')`)
+    .then(()=>{console.log("New user Created ");res.json({message:"New user Successfully created"})})
     .catch((e)=>{console.error(e.message);res.json({message:e.message})});
     }
     catch(e){
@@ -31,23 +41,50 @@ authRouter.post("/login",async(req,res)=>{
     if(!data){
         return res.json({message:"Error!Cannot get the data"});
     }
-    if(!data.email.trim() ||!data.password.trim()){
+    if(!data.phone.trim() ||!data.password.trim()){
         return res.json({message:"Fill all the fields"})
     }
-    const result = await connections.query(`Select password from users where email='${data.email}';`);
+    const result = await connections.query(`Select password from users where id='${data.phone}';`);
     const password = result.rows[0]?.password;
-    if(!password) res.json({message:"Email or Password Invalid"});
+    if(!password) return res.json({message:"Phone or Password Invalid"});
+    if(password===process.env.DEFAULTPASSWORD && data.password===password){
+        let result = await connections.query(`
+                UPDATE users 
+                SET allowChangePassword = 'true' 
+                WHERE id = '${data.phone}'`);;
+        return res.json({message:"Default password detected,Kindly change the password.You can change the password by logging in with the password you wish to change"});
+        
+    }
+    else if(password===process.env.DEFAULTPASSWORD && data.password !== password){
+        let result = await connections.query(`select allowChangePassword from users where id=${data.phone}`);
+        let canChange = result.rows[0]?.allowchangepassword;
+        console.log(canChange);
+        if(canChange==='true'){
+            const salt =bcrypt.genSaltSync(Number(process.env.bcryptKey));
+            const hashed = bcrypt.hashSync(data.password,salt);
+
+            result = await connections.query(`update users 
+                SET password='${hashed}',
+                allowchangepassword='false'
+                 where id='${data.phone}'`);
+            return res.json({message:"Changed Password"});
+        }
+        return res.json({message:"Wrong Password"});
+    }
+    else{
     const isCorrect = bcrypt.compareSync(data.password,password);
     if(isCorrect){
        return res.json({message:"welcome user"});
     }
-    res.json({message:"Email or Password Invalid"});
+    res.json({message:"Phone or Password Invalid"});}
+
 })
 
 authRouter.post("/startwork",async(req,res)=>{
     const data = req.body;
     if(!data) return res.json({message:"Data not Fetched"});
     if(!data.id)return res.json({message:"id cannot be fetched"});
+    console.log(data.id);
     try{
         const result = await connections.query(`insert into attendance(user_id) Values(${data.id});`);
     }

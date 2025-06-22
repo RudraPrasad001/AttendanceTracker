d12320da4346c1e963ace81e10c6b56feacb4ade
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-06-22 15:09:29 +0530

    Location Shi

--- db/tableInitialization.js
+++ db/tableInitialization.js
@@ -11,6 +11,11 @@ try{
     connections.query(attendanceSql)
      .then(() => console.log('✅ Attendance file executed'))
     .catch(err => console.error('❌ Error executing SQL file:', err))
+
+    const locationSql = fs.readFileSync('./schema/location.sql').toString();
+    connections.query(locationSql)
+     .then(() => console.log('✅ Location file executed'))
+    .catch(err => console.error('❌ Error executing SQL file:', err))
 }
 catch(e){
     console.error(e);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,37 +4,7 @@ import connections from "../db/dbConnection.js";
 import {config} from "dotenv";
 config();
 const authRouter = express.Router();
-authRouter.post("/register",async(req,res)=>{
-    const data = req.body;
-    if(!data){
-        return res.json({message:"Error!Cannot get the data"});
-    }
-    if(!data.name.trim() || !data.phone.trim()||data.phone.length!==10){
-        return res.json({message:"Fill all the fields Correctly"});
-    }
-      //trying to see if the user already exists with the phone number;
-    try{
-        let query = `select id from users where id=${data.phone}`;
-        const result = await connections.query(query);
-        if(!result) throw new Error("Something broke");
-        if(result.rows[0]?.id){{throw new Error("Id already registered")}}
-    }
-    catch(e){
-        console.error(e.message);
-        return;
-    }
-    const defaultPassword = process.env.DEFAULTPASSWORD;
-  
-    try{
-    connections.query(`insert into users(id,name,password) values('${new Number(data.phone)}','${data.name}','${defaultPassword}')`)
-    .then(()=>{console.log("New user Created ");res.json({message:"New user Successfully created"})})
-    .catch((e)=>{console.error(e.message);res.json({message:e.message})});
-    }
-    catch(e){
-        console.error(e);
-    }
 
-})
 
 authRouter.post("/login",async(req,res)=>{
     const data = req.body;

bc5cdd3480f909a655899969869d524218b2ab0f
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-06-25 22:18:03 +0530

    Weekly Leave Tracker

--- schema/attendance.sql
+++ schema/attendance.sql
@@ -1,5 +1,8 @@
 CREATE TABLE IF NOT EXISTS attendance (
     id SERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES users(id),
-    entrytime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    entrytime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_holiday BOOLEAN,
+    pay_day BOOLEAN,
+    is_present BOOLEAN
 );
